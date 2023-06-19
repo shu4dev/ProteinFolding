e1815f2c64aec6913f22351c0756ae87ee37a126
Brian Shu <shu4@hawaii.edu>
2023-06-19 09:06:44 -1000

    first commit

---
+++ ProteinRec.py
@@ -0,0 +1,26 @@
+def CreateBoard(Acid):
+    length = len(Acid) * 2
+    arr = [[0] * length] * length
+    return arr
+
+
+
+
+def check(row, col):
+    point = 0
+    return point
+
+def Route(Board, AcidNum):
+    
+    return 0
+
+def driver(Acid):
+    board = CreateBoard(Acid)
+    Route(board, Acid)
+    return board
+
+print(driver([0,1,1,0]))
+        
+
+
+
