16c8dc2441e0fd0e2900fc4f0a4d8b1dbe9aa8ff
Brian_Shu <briandev7315@gmail.com>
2023-06-24 18:47:09 -1000

    90% done

--- ProteinRec.py
+++ ProteinRec.py
@@ -1,47 +1,56 @@
-solution = []   
-    
-def CreateBoard(Acid):
-    length = (len(Acid) * 2) + 1
-    arr = [[" " for i in range(length)] for j in range(length)]
-    return arr
+input  = [0,1,1,0]
+grid   = [[" " for i in range(2 * len(input) + 1)] for j in range(2 * len(input) + 1)]
+Maxpts = 0
+grid[4][4] = input[0]
 
 def print2D(array):
     for row in array:
         print(row)
+
 def Checkpts(grid, x, y):
     if (grid[x][y] == 0 and grid[x + 1][y] == 0):
         return 1
     elif (grid[x][y] == 0 and grid[x - 1][y] == 0):
         return 1
-    elif (grid[x][y] == 0 and grid[x ][y + 1] == 0):
+    elif (grid[x][y] == 0 and grid[x][y + 1] == 0):
         return 1
     elif (grid[x][y] == 0 and grid[x][y - 1] == 0):
         return 1  
     return 0
 
 def isValid(grid, x, y):
-    if grid[x + 1][y] != " " and grid[x - 1][y] != " " and grid[x][y + 1] and grid[x][y - 1]:
+    if grid[x][y] != " ":
         return False
     return True
 
 def ProteinFolding(Acidseq, grid, Curnum, x_move, y_move, x, y, pts):
+
     if Curnum == len(Acidseq):
-        return True
+        return pts
+    
     for i in range(4):
+        
         x_new = x + x_move[i]
         y_new = y + y_move[i]
     
     if isValid(grid, x_new, y_new):
-        grid[x_new][y_new] = Acidseq[i]
+
+        pts_new = pts + Checkpts(grid, x_new, y_new)
+        grid[x_new][y_new] = Acidseq[Curnum]
+
+        if ProteinFolding(Acidseq, grid, Curnum + 1, x_move, y_move, x_new, y_new, pts_new) > 0:
+            print()
+            print2D(grid)            
+            return True
+        grid[x_new][y_new] = " "
+        
 
     return False
 
-def solver(Acidseq):
-    x_cen, y_cen = len(Acidseq), len(Acidseq)
-    board = CreateBoard(Acidseq)
-    board[x_cen][y_cen] = Acidseq[0]
-    
-    return 0
 
 if __name__ == "__main__":
-    solver([0,1,1,0])
+
+    x_move = [-1,1,0,0]
+    y_move = [0,0,-1,1]
+
+    ProteinFolding(input, grid, 1, x_move, y_move, 4, 4 ,0)
